3b4383bdb9ce72d5069b2913852104217d365684
shinjun14 <shinjunsakano@gmail.com>
2021-09-12 07:27:33 +0000

    [Start]4-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
   root to: 'homes#top'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
